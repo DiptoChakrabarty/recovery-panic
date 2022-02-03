58824186f665cf1671761b904ae7c69c8b229742
DiptoChakrabarty <diptochuck123@gmail.com>
2022-02-03 19:08:37 +0530

    panic and normal page

--- main.go
+++ main.go
@@ -12,11 +12,18 @@ func main() {
 
 	defaultRoutes := router.Group("/")
 	{
-		defaultRoutes.GET("", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"Message": "Home Page",
-			})
-		})
+		defaultRoutes.GET("", defaultPage)
+		defaultRoutes.GET("/panic", panicPage)
 	}
 	router.Run(":5000")
+}
+
+func defaultPage(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"Message": "Home Page",
+	})
+}
+
+func panicPage(ctx *gin.Context) {
+	panic("Crashed Application")
 }
